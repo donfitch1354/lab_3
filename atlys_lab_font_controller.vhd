c4fde67e0e11df34dff5b8d8790806315d30580f
Ethan Snyder <ethan.snyder@usafa.edu>
2014-03-03 13:54:22 -0700

    cleaning up code and compiling to test

--- atlys_lab_font_controller.vhd
+++ atlys_lab_font_controller.vhd
@@ -22,27 +22,171 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+library UNISIM;
+use UNISIM.VComponents.all;
 
 entity atlys_lab_font_controller is
     Port ( clk : in  STD_LOGIC;
            reset : in  STD_LOGIC;
            start : in  STD_LOGIC;
-           witch : in  STD_LOGIC;
-           led : out  STD_LOGIC;
-           tmds : out  STD_LOGIC;
-           tmdsb : out  STD_LOGIC);
+           switch : in  STD_LOGIC_VECTOR (7 downto 0);
+           led : out  STD_LOGIC_VECTOR (7 downto 0);
+           tmds : out  STD_LOGIC_VECTOR (3 downto 0);
+           tmdsb : out  STD_LOGIC_VECTOR (3 downto 0));
 end atlys_lab_font_controller;
 
 architecture Behavioral of atlys_lab_font_controller is
 
+	signal  blue_s, red_s, green_s, enable,  pixel_clk,  v_sync_sig, 
+	v_sync_sig_1, v_sync_sig_2, h_sync_sig, h_sync_sig_1, h_sync_sig_2, 
+	v_completed_sig, blank, clock_s, serialize_clk, serialize_clk_n: std_logic;  
+	signal   red, green, blue, ascii: std_logic_vector (7 downto 0); 
+	signal column_connection, row_connection, row_sig, row_sig_1, row_sig_2, column_sig, column_sig_1, column_sig_2: std_logic_vector (10 downto 0); 
+	signal blank_sig, blank_sig_1, blank_sig_2: std_logic; 
+	
+	
+COMPONENT character_gen 
+	PORT(
+			 clk : in  STD_LOGIC;
+			 reset: in STD_LOGIC; 
+           blank : in  STD_LOGIC;
+           row : in std_logic_vector (10 downto 0);
+           column : in  std_logic_vector (10 downto 0);
+           ascii_to_write : in  std_logic_vector (7 downto 0);
+           write_en : in  STD_LOGIC;
+           r,g,b : out  std_logic_vector (7 downto 0)
+			  );
+END COMPONENT; 
+
+component vga_sync 
+     
+     Port ( clk : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           h_sync : out  STD_LOGIC;
+           v_sync : out  STD_LOGIC;
+           v_completed : out  STD_LOGIC;
+           blank : out  STD_LOGIC;
+           row :  out std_logic_vector(10 downto 0);
+           column :  out std_logic_vector(10 downto 0));
+  end component vga_sync ;
+
 begin
 
+    -- Clock divider - creates pixel clock from 100MHz clock
+    inst_DCM_pixel: DCM
+    generic map(
+                   CLKFX_MULTIPLY => 2,
+                   CLKFX_DIVIDE   => 8,
+                   CLK_FEEDBACK   => "1X"
+               )
+    port map(
+                clkin => clk,
+                rst   => reset,
+                clkfx => pixel_clk
+            );
+
+    -- Clock divider - creates HDMI serial output clock
+    inst_DCM_serialize: DCM
+    generic map(
+                   CLKFX_MULTIPLY => 10, -- 5x speed of pixel clock
+                   CLKFX_DIVIDE   => 8,
+                   CLK_FEEDBACK   => "1X"
+               )
+    port map(
+                clkin => clk,
+                rst   => reset,
+                clkfx => serialize_clk,
+                clkfx180 => serialize_clk_n
+            );
+
+    -- TODO: VGA component instantiation
+	
+
+	  Inst_vga_sync: vga_sync PORT MAP(
+				clk => pixel_clk,
+				reset => reset, 
+				h_sync => h_sync_sig, 
+				v_sync=> v_sync_sig, 
+				v_completed =>  v_completed_sig, 
+				blank => blank_sig, 
+				row => row_sig,
+				column => column_sig
+	         																						
+				);
+		Inst_character_gen: character_gen PORT MAP (
+					clk => pixel_clk,
+					blank => blank_sig,
+					reset => reset, 
+					row => std_logic_vector(row_connection),
+					column=> std_logic_vector (column_connection),
+					ascii_to_write => ascii, 
+					write_en => enable, 
+					r => red,
+					g => green, 
+					b => blue
+					);
+					
+	
+				
+	 
+
+    inst_dvid: entity work.dvid 
+    port map(
+                clk       => serialize_clk,
+                clk_n     => serialize_clk_n, 
+                clk_pixel => pixel_clk,
+                red_p     => red,
+                green_p   => green,
+                blue_p    => blue,
+                blank     => blank_sig_2,
+                hsync     => h_sync_sig_2,
+                vsync     => v_sync_sig_2,
+                -- outputs to TMDS drivers
+                red_s     => red_s,
+                green_s   => green_s,
+                blue_s    => blue_s,
+                clock_s   => clock_s
+            );
+
+    -- Output the HDMI data on differential signalling pins
+    OBUFDS_blue  : OBUFDS port map
+        ( O  => TMDS(0), OB => TMDSB(0), I  => blue_s  );
+    OBUFDS_red   : OBUFDS port map
+        ( O  => TMDS(1), OB => TMDSB(1), I  => green_s );
+    OBUFDS_green : OBUFDS port map
+        ( O  => TMDS(2), OB => TMDSB(2), I  => red_s   );
+    OBUFDS_clock : OBUFDS port map
+        ( O  => TMDS(3), OB => TMDSB(3), I  => clock_s );
+		  
+		  
+		  
+		  
+		  
+		  --- adding upper level delays (2x): 
+		  process( pixel_clk)
+				begin 
+					if (rising_edge(pixel_clk)) then 
+						blank_sig_1 <= blank_sig;
+						h_sync_sig_1 <= h_sync_sig; 
+						v_sync_sig_1 <= v_sync_sig; 
+					end if;
+			end process; 
+			
+			process (pixel_clk)
+				begin 
+					if (rising_edge(pixel_clk)) then 
+						blank_sig_2 <= blank_sig_1;
+						h_sync_sig_2 <= h_sync_sig_1; 
+						v_sync_sig_2 <= v_sync_sig_1; 
+					end if; 
+			end process; 
+		  
 
 end Behavioral;
+
+
 
--- character_gen.vhd
+++ character_gen.vhd
@@ -29,11 +29,12 @@ use UNISIM.VComponents.all;
 entity character_gen is
     Port ( clk : in  STD_LOGIC;
            blank : in  STD_LOGIC;
-           row : in  unsigned (10 downto 0);
-           column : in  unsigned (10 downto 0);
-           ascii_to_write : in  unsigned (7 downto 0);
+           row : in  std_logic_vector (10 downto 0);
+           column : in  std_logic_vector (10 downto 0);
+           ascii_to_write : in  std_logic_vector (7 downto 0);
            write_en : in  STD_LOGIC;
-           r,g,b : out  std_logic_vector (7 downto 0)
+           r,g,b : out  std_logic_vector (7 downto 0);
+			  reset: in std_logic
 			  );
 end character_gen;
 
@@ -52,8 +53,8 @@ END COMPONENT;
 COMPONENT font_rom
 PORT (
 			clk : in std_logic;
-			addr : in unsigned (10 downto 0); 
-			data: out unsigned ( 7 downto 0)  
+			addr : in std_logic_vector (10 downto 0); 
+			data: out std_logic_vector ( 7 downto 0)  
 		); 
 END COMPONENT;
 
@@ -69,17 +70,18 @@ PORT (
          data_out_b : out std_logic_vector(7 downto 0) 
 			);
 END COMPONENT;
-signal mux_output: std_logic; 
+signal mux_output, write_enabled, output : std_logic; 
 signal data_font: unsigned (7 downto 0); 
-signal buffer_out_a, buffer_out_b: std_logic_vector (7 downto 0); 
+signal buffer_out_a, buffer_out_b, font_data: std_logic_vector (7 downto 0); 
 signal count, count_next: std_logic_vector (11 downto 0); 
-signal address_signal, address: std_logic_vector (10 downto 0);
-signal row_next, row_reg: std_logic_vector (3 downto 0); 
-signal column_reg, column_next, column_next_next: std_logic_vector (2 downto 0); 
+signal address_signal, address, font_addr: std_logic_vector (10 downto 0);
+signal row_sig, row_sig_next: std_logic_vector (3 downto 0); 
+signal col_sig, col_sig_next, sel: std_logic_vector (2 downto 0); 
+signal address_b_12: std_logic_vector (11 downto 0); 
+signal address_b_sig: std_logic_vector ( 13 downto 0); 
+signal data_b_sig: std_logic_vector (7 downto 0);
+signal font_add: std_logic_vector (10 downto 0); 
 
-
-signal row_col: std_logic_vector (13 downto 0);
-signal row_col_2 : std_logic_vector (11 downto 0); 
 
 
 begin
@@ -88,7 +90,7 @@ Inst_char_screen_buffer: char_screen_buffer PORT MAP(
 			clk => clk, 
 			we => write_enabled,
 			address_a => count,
-			address_b => row_col_multiply_12,
+			address_b => address_b_12,
 			data_in => ascii_to_write,
 			data_out_a => open,
 			data_out_b => data_b_sig
@@ -96,40 +98,106 @@ Inst_char_screen_buffer: char_screen_buffer PORT MAP(
 			
 Inst_font_rom: font_rom PORT MAP (
 			clk => clk, 
-			addr => address_signal,
+			addr => font_addr,
 			data => font_data
 			); 
 			
 
-Inst_mux: mux PORT MAP (
-			data => font_data, 
-			sel => col_next_2, 
-			output => mux_output
-			);
 
-count <= std_logic_vector( unsigned (count_next) + 1) when rising_edge(write_en) else
+
+
+	count <= std_logic_vector( unsigned (count_next) + 1) when rising_edge(write_en) else
 						count_next; 
 						
-count_next <= (others => '0') when reset = '1' 
+	count_next <= (others => '0') when reset = '1' 
 		else 
 			count; 
 
+	sel <= col_sig_next(2 downto 0);
+
+	process (font_data, sel)
+	begin 
+		case sel is 
+				when "000" => 
+					output <= font_data(7);
+				when "001" => 
+					output <= font_data(6);
+				when "010" => 
+					output <= font_data(5);	
+				when "011" => 
+					output <= font_data(4);	
+				when "100" => 
+					output <= font_data(3);
+				when "101" => 
+					output <= font_data(2);
+				when "110" => 
+					output <= font_data(1);
+				when "111" => 
+					output <= font_data(0);
+		end case; 
+	end process; 
+
+-- insert processes for row_col, row_col2, etc
+font_add <= data_b_sig(6 downto 0) & row_sig; 
+
+
+	r <= (others => '1') when output = '1' else 
+			(others => '0');
+			
+
+	g <= (others => '0'); 
+	r <= (others => '0'); 
+	
+	
+	
+	address_b_sig <=  std_logic_vector(((unsigned(column(10 downto 3)))+unsigned (row(10 downto 4))*80));
+	address_b_12 <= address_b_sig (11 downto 0); 
+	
+	
+	
+	
+	
+	
+	process(clk)
+		begin 
+			if (rising_edge(clk)) then 
+				row_sig <= row(3 downto 0); 
+				col_sig <= column(2 downto 0); 
+			end if; 
+	end process; 
+	
+	process (clk)
+		begin 
+			if (rising_edge(clk)) then 
+				col_sig_next <= col_sig (2 downto 0);
+			end if; 
+	end process; 
+	
+	
 
 
 
 
-process (mux_output)
-begin 
-	if (mux_output = '1') then 
-		r <= (others => '1');
-		g <= (others => '1'); 
-		b <= (others => '1');
-	else
-		r <= (others => '0'); 
-		g <= (others => '0');
-		b <= (others => '0'); 
-	end if; 
-end process; 
+
+
+
+
+
+
+--not using mux on first run
+
+--process (mux_output)
+--begin 
+--	if (mux_output = '1') then 
+	--	r <= (others => '1');
+	--	g <= (others => '1'); 
+	--	b <= (others => '1');
+--	else
+--		r <= (others => '0'); 
+--		g <= (others => '0');
+--		b <= (others => '0'); 
+--	end if; 
+--end process; 
 
 
 
